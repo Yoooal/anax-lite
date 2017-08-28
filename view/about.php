4f16cd0a9ab0a13a95c2453a5dbc0f58cc2c762a
Joel Pettersson <Joel.pettersson87@gmail.com>
2017-08-28 11:29:30 +0200

    Kmom04: Lagra innehåll i databasen, update

--- view/about.php
+++ view/about.php
@@ -1,4 +1,22 @@
-<?php $urlReport = $app->url->create("status"); ?>
+<?php
+$db = $app->db;
+$urlReport = $app->url->create("status");
+
+$sql = <<<EOD
+SELECT
+*
+FROM content
+WHERE
+path = ?
+AND type = ?
+AND (deleted IS NULL OR deleted > NOW())
+AND published <= NOW()
+;
+EOD;
+
+$content = $db->executeFetch($sql, ["aboutText", "block"]);
+
+?>
 
 <div class="container" role="main">
     <div class="page-header">
@@ -10,20 +28,7 @@
       </div>
       <div class="col-md-9 bak">
         <br>
-          Hej mitt namn är Joel Pettersson.
-          Jag är 29 år och bor i Stockholm.
-          Är uppväxt i Göteborg men flyttade till Stockholm för
-          ungefär två år sedan, och trivs hittills bra i storstan.
-          Har precis påbörjat en ny utbildning inom webbprogrammering på
-          Blekinge Tekniska Högskola och ser fram emot vad kursen har
-          att erbjuda mig!
-          För övrigt är jag en glad, positiv och social kille som gillar
-          att möta nytt folk och umgås med mina vänner.
-          Det var kul att ni kom in på min hemsida.
-          Hoppas ni gillar den!
-          Objektorienterad PHP ska bli spännande och jag ser fram emot den här kursen!<br>
-          Repo på github: <a href="https://github.com/Yoooal/anax-lite">Anax-lite</a><br>
-          <a href="<?= $urlReport ?>">Status</a>
+        <?= $content->data ?>
       </div>
     </div>
 </div>
